c58bb75b463714d006e07944f70d42464dd66b10
Shawn <shawnstark@mail.gnic.io>
2016-06-04 13:37:32 -0500

    first commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,81 +1,178 @@
-/*
-Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
-*/
+/*#############
+For convenience
+###########*/
+function addKeyVal(key, val, obj){
+      //requires that an object is passed in.
+      obj[key] = val;
+    }    
+function emptyObj(){}
+/*##########################################
+Helper functions and Functions for when
+it works differently in different browsers
+##########################*/
+  function getPosition(el) {
+  //Find our more about this function here
+  //https://www.kirupa.com/html5/get_element_position_using_javascript.htm
+    var _top = 0;var _left = 0;var _bottom = 0;var _right = 0;var rect = null;
+    if(rect=el.getBoundingClientRect()){return rect;}else{
+      while(el){
+        if (el.tagName == "BODY") {
+          var xScroll = el.scrollLeft || document.documentElement.scrollLeft;
+          var yScroll = el.scrollTop || document.documentElement.scrollTop;     
+          _left += (el.offsetLeft - xScroll + el.clientLeft);
+          _top += (el.offsetTop - yScroll + el.clientTop);
+        }else{
+          _left += (el.offsetLeft - el.scrollLeft + el.clientLeft);
+          _top += (el.offsetTop - el.scrollTop + el.clientTop);
+          _bottom = el.offsetHeight+_top;
+          _right = el.offsetWidth+_left;
+        }
+        el = el.offsetParent;
+      }
+      return {top: _top,left: _left,bottom: _bottom,right: _right};
+    }
+    return null;
+  }
+   /*##############*/
+  function getViewport() {
+  //Find out more about this function here
+  // http://andylangton.co.uk/articles/javascript/get-viewport-size-javascript/ 
+     var viewPortWidth;
+     var viewPortHeight;
+     if (typeof window.innerWidth != 'undefined') {
+       viewPortWidth = window.innerWidth,
+       viewPortHeight = window.innerHeight
+     }
+     else if (typeof document.documentElement != 'undefined'
+     && typeof document.documentElement.clientWidth !=
+     'undefined' && document.documentElement.clientWidth != 0) {
+        viewPortWidth = document.documentElement.clientWidth,
+        viewPortHeight = document.documentElement.clientHeight
+     }
+     else {
+       viewPortWidth = document.getElementsByTagName('body')[0].clientWidth,
+       viewPortHeight = document.getElementsByTagName('body')[0].clientHeight
+     }
+     return {width:viewPortWidth, height:viewPortHeight};
+  }
+/*##########################################
+Functions tied specifically to this page 
+dependent on element name, id, class and/or 
+location of elements on the page.
+##########################*/
+  function updatePosition() {
+        /*##################
+        Index the elements we need
+        ########################*/
+        Navigator = document.querySelector("#navigator");
+        TitleBar = document.querySelector("#title-bar");
+        PageContainer = document.querySelector("#page-container");
+        CompanyName = document.querySelector("#companyname");
+        Outro = document.querySelector('#outro');
+        OutroSpan = document.querySelector('#thankyou');
+        /*##################
+        Take a snap shot of indexed elements 
+        cur location relative to viewport
+        ########################*/
+        CompanyNameRect = getPosition(CompanyName);
+        NavigatorRect = getPosition(Navigator);
+        TitleBarRect = getPosition(TitleBar);
+        OutroRect = getPosition(Outro);
+        OutroSpanRect = getPosition(OutroSpan);
 
-(function(document) {
-  'use strict';
-
-  // Grab a reference to our auto-binding template
-  // and give it some initial binding values
-  // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
-  var app = document.querySelector('#app');
-  
-  // Sets app default base URL
-  app.baseUrl = '/';
-  if (window.location.port === '') {  // if production
-    // Uncomment app.baseURL below and
-    // set app.baseURL to '/your-pathname/' if running from folder in production
-    // app.baseUrl = '/polymer-starter-kit/';
+        NavigatorCurPositionProp = getComputedStyle(Navigator).getPropertyValue("position");
+        if(NavigatorRect.top <= CompanyNameRect.bottom && NavigatorCurPositionProp == "relative"){
+          Navigator.style.position = "fixed";
+          adjustedPosition = CompanyNameRect.bottom;
+          Navigator.style.top = adjustedPosition+"px";
+          Navigator.style.left = "0px";
+          OutroHeight = OutroRect.bottom - OutroRect.top;
+          OutroSpanHeight = OutroSpanRect.bottom - OutroSpanRect.top;
+          OutroSpan.classList.remove('verticalTranslateDown');
+          OutroSpan.classList.add('verticalTranslateUp');
+          //Outro.style.background = "rgba(40, 87, 40, 1)";
+        }
+        if(TitleBarRect.bottom >= NavigatorRect.top && NavigatorCurPositionProp == "fixed"){
+            /*################################################
+            #If the element affected here is not soley wrapped in an element that has a fixed size and 
+            #maintains flow this request for layout and paint caused by the style changes could cause
+            #a domino effect -- Note worthy if you need to do anything more drastic than this be careful
+            ############################################*/
+            Navigator.style.position = "relative";
+            Navigator.style.top = "0px";
+            Navigator.style.left = "0px";
+            //Outro.style.background = "transparent";
+            OutroSpan.classList.remove('verticalTranslateUp'); 
+            OutroSpan.classList.add('verticalTranslateDown');   
+        }
+  }
+  function setBodyPaddingRelViewPort(){
+       Body = document.querySelector("body");
+       ViewPort = getViewport();
+       Padding=(ViewPort.height*.5);
+       Body.style.paddingBottom = Padding+"px";
   }
 
-  app.displayInstalledToast = function() {
-    // Check to make sure caching is actually enabled—it won't be in the dev environment.
-    if (!Polymer.dom(document).querySelector('platinum-sw-cache').disabled) {
-      Polymer.dom(document).querySelector('#caching-complete').show();
-    }
-  };
+  function updateSize(){
+      dealWithCSSQuirks();
+      setBodyPaddingRelViewPort();
+      updatePosition();
+  }
+  function dealWithCSSQuirks(){
+    //This function sets a css custom property to change its default
+    //bottom position, should run only on screen resize and load
+    Root = document.body; 
+    ThankYou = document.querySelector('#thankyou');
+    ThankYouRect = getPosition(ThankYou);
+    ThankYouHeight = ThankYouRect.bottom - ThankYouRect.top+20;
+    ThankYouNewBottom = 0 - ThankYouHeight;
+    ThankYouNewBottom += "px";
+    ThankYou.style.setProperty('height',ThankYouHeight);
+    ThankYou.style.setProperty('bottom',ThankYouNewBottom);
+  }
 
-  // Listen for template bound event to know when bindings
-  // have resolved and content has been stamped to the page
-  app.addEventListener('dom-change', function() {
-    console.log('Our app is ready to rock!');
-  });
-  
-  // See https://github.com/Polymer/polymer/issues/1381
-  window.addEventListener('WebComponentsReady', function() {
-    // imports are loaded and elements have been registered
-  });
+(function(document) {
+      'use strict';
+      /*#######################################################
+      Grab a reference to our auto-binding template
+      and give it some initial binding values
+      Learn more about auto-binding templates at http://goo.gl/Dx1u2g
+      #############################################*/
+      var app = document.querySelector('#app');
+        window.addEventListener("scroll", updatePosition, false);
+        window.addEventListener("resize", updateSize, false);
+      // Sets app default base URL
+      app.baseUrl = '/';
+      if (window.location.port === '') {  // if production
+        // Uncomment app.baseURL below and
+        // set app.baseURL to '/your-pathname/' if running from folder in production
+        // app.baseUrl = '/polymer-starter-kit/';
+      }
 
-  // Main area's paper-scroll-header-panel custom condensing transformation of
-  // the appName in the middle-container and the bottom title in the bottom-container.
-  // The appName is moved to top and shrunk on condensing. The bottom sub title
-  // is shrunk to nothing on condensing.
-  window.addEventListener('paper-header-transform', function(e) {
-    var appName = Polymer.dom(document).querySelector('#mainToolbar .app-name');
-    var middleContainer = Polymer.dom(document).querySelector('#mainToolbar .middle-container');
-    var bottomContainer = Polymer.dom(document).querySelector('#mainToolbar .bottom-container');
-    var detail = e.detail;
-    var heightDiff = detail.height - detail.condensedHeight;
-    var yRatio = Math.min(1, detail.y / heightDiff);
-    // appName max size when condensed. The smaller the number the smaller the condensed size.
-    var maxMiddleScale = 0.50;
-    var auxHeight = heightDiff - detail.y;
-    var auxScale = heightDiff / (1 - maxMiddleScale);
-    var scaleMiddle = Math.max(maxMiddleScale, auxHeight / auxScale + maxMiddleScale);
-    var scaleBottom = 1 - yRatio;
+      app.displayInstalledToast = function() {
+        // Check to make sure caching is actually enabled—it won't be in the dev environment.
+        if (!Polymer.dom(document).querySelector('platinum-sw-cache').disabled) {
+          Polymer.dom(document).querySelector('#caching-complete').show();
+        }
+      };
 
-    // Move/translate middleContainer
-    Polymer.Base.transform('translate3d(0,' + yRatio * 100 + '%,0)', middleContainer);
+      // Listen for template bound event to know when bindings
+      // have resolved and content has been stamped to the page
+      app.addEventListener('dom-change', function() {
+        console.log('Our app is ready to rock!');
+      });
 
-    // Scale bottomContainer and bottom sub title to nothing and back
-    Polymer.Base.transform('scale(' + scaleBottom + ') translateZ(0)', bottomContainer);
 
-    // Scale middleContainer appName
-    Polymer.Base.transform('scale(' + scaleMiddle + ') translateZ(0)', appName);
-  });
-
-  // Scroll page to top and expand header
-  app.scrollPageToTop = function() {
-    app.$.headerPanelMain.scrollToTop(true);
-  };
-
-  app.closeDrawer = function() {
-    app.$.paperDrawerPanel.closeDrawer();
-  };
-
+      // See https://github.com/Polymer/polymer/issues/1381
+      window.addEventListener('WebComponentsReady', function() {
+        // imports are loaded and elements have been registered
+        setBodyPaddingRelViewPort();
+        dealWithCSSQuirks();
+	//console.log('WebComponentsReady Fires here');
+        //updatePosition();
+      });
+      /*******************
+        PSK had additional script here.
+        Reference the original project.
+      ***************/
 })(document);
--- polymer-starter-kit-1.3.0/app/scripts/app.js
+++ polymer-starter-kit-1.3.0/app/scripts/app.js
@@ -53,14 +53,14 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
     var heightDiff = detail.height - detail.condensedHeight;
     var yRatio = Math.min(1, detail.y / heightDiff);
     // appName max size when condensed. The smaller the number the smaller the condensed size.
-    var maxMiddleScale = 0.50;
+    var maxMiddleScale = .50;
     var auxHeight = heightDiff - detail.y;
     var auxScale = heightDiff / (1 - maxMiddleScale);
     var scaleMiddle = Math.max(maxMiddleScale, auxHeight / auxScale + maxMiddleScale);
     var scaleBottom = 1 - yRatio;
 
     // Move/translate middleContainer
-    Polymer.Base.transform('translate3d(0,' + yRatio * 100 + '%,0)', middleContainer);
+    //Polymer.Base.transform('translate3d(0,' + yRatio * 100 + '%,0)', middleContainer);
 
     // Scale bottomContainer and bottom sub title to nothing and back
     Polymer.Base.transform('scale(' + scaleBottom + ') translateZ(0)', bottomContainer);
